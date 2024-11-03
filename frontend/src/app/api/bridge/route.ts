bacb210c3507618ff4ff6ca1b45a87b707526ffd
Claudio Condor <claucondor@gmail.com>
2024-11-03 17:40:51 -0400

    fix duration

--- frontend/src/app/api/bridge/route.ts
+++ frontend/src/app/api/bridge/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { AuthService, NetworkService, BridgeService } from "./services";
 import { formatEther } from "viem";
-export const maxDuration = 120;
+export const maxDuration = 60;
 
 export async function POST(request: Request) {
   try {
