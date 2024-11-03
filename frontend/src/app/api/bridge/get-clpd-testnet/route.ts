bf474a413ceb76a347e224837870f8119adb9156
Claudio Condor <claucondor@gmail.com>
2024-11-02 22:41:47 -0400

    feat(fauced): endpoint for CLPD test fauced ready

--- frontend/src/app/api/bridge/get-clpd-testnet/route.ts
+++ frontend/src/app/api/bridge/get-clpd-testnet/route.ts
@@ -10,7 +10,7 @@ import { addresses } from "@/constants/address";
 import { NextResponse } from "next/server";
 
 // crypto
-import { ethers } from "ethers";
+import { ethers, parseUnits } from "ethers";
 
 // viem
 import { formatEther } from "viem";
@@ -27,7 +27,14 @@ export async function POST(request: Request) {
 
   try {
     const FAUCET_MAX_BALANCE = 100;
-    // Check if user has enough CLPD balance
+    const FAUCET_AMOUNT = 10; // cantidad de CLPD a enviar
+    const pkRechargeEthCldp = process.env.PK_RECHARGE_ETH_CLPD;
+
+    if (!pkRechargeEthCldp) {
+      console.log("❌ PK_RECHARGE_ETH_CLPD not found");
+      return NextResponse.json({ error: "❌ PK_RECHARGE_ETH_CLPD not found" }, { status: 400 });
+    }
+
     const provider = new ethers.JsonRpcProvider(baseSepolia.rpcUrls.default.http[0]);
     const contract = new ethers.Contract(addresses.baseSepolia.CLPD.address, CLPD_ABI, provider);
     const balance = await contract.balanceOf(userAddress);
@@ -39,7 +46,23 @@ export async function POST(request: Request) {
       return NextResponse.json({ error: "❌ Sufficient CLPD balance" }, { status: 400 });
     }
 
-    // TODO: faucet clpd
+    // Configurar wallet y contrato para enviar CLPD
+    const wallet = new ethers.Wallet(pkRechargeEthCldp, provider);
+    const contractWithSigner: any = contract.connect(wallet);
+    
+    const amountToSend = parseUnits(FAUCET_AMOUNT.toString(), addresses.base.CLPD.decimals);
+
+    try {
+      const tx = await contractWithSigner.transfer(userAddress, amountToSend);
+      console.log("🧾 Transaction sent, waiting confirmation...");
+      console.log("🧾 Transaction hash:", tx.hash);
+      await tx.wait();
+
+      console.log("✅ CLPD faucet transfer confirmed");
+    } catch (error) {
+      console.error("❌ Transaction failed:", error);
+      return NextResponse.json({ error: "❌ Transaction failed" }, { status: 400 });
+    }
 
     return NextResponse.json({ message: "✅ Transfer completed successfully" }, { status: 200 });
   } catch (error) {
